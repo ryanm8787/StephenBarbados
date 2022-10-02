b48f60dbf669589a552303d396226758c61ed883
ryanm8787 <rmcdonald19@qub.ac.uk>
2022-10-02 18:49:45 +0100

    'initdec

---
+++ StephSrc/Assets/Scripts/scr_player_movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_player_movement : MonoBehaviour
+{
+    public float speed = 2;
+
+    public float rotationSpeed = 720;
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        if (Input.GetKey("shift"))
+        {
+            Debug.Log("shift pressed :)");
+        }
+
+        Vector2 movementDirection = new Vector2(horizontalInput, verticalInput);
+        float inputMagnitude = Mathf.Clamp01(movementDirection.magnitude);
+        movementDirection.Normalize();
+
+        transform.Translate(movementDirection * speed * inputMagnitude * Time.deltaTime, Space.World);
+
+        if (movementDirection != Vector2.zero)
+        {
+            Quaternion toRotation = Quaternion.LookRotation(Vector3.forward, movementDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, toRotation, rotationSpeed * Time.deltaTime);
+        }
+    }
+}
